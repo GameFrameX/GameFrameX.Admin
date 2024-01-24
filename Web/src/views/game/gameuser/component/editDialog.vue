6b0de0915f63af8184496e730d72093593762e83
Blank <wangfj11@foxmail.com>
2024-01-24 16:40:49 +0800

    [增加]1. 增加前端的玩家模块

---
+++ Web/src/views/game/gameuser/component/editDialog.vue
@@ -0,0 +1,139 @@
+﻿<template>
+	<div class="gameUserEntity-container">
+		<el-dialog v-model="isShowDialog" :width="800" draggable="">
+			<template #header>
+				<div style="color: #fff">
+					<!--<el-icon size="16" style="margin-right: 3px; display: inline; vertical-align: middle"> <ele-Edit /> </el-icon>-->
+					<span>{{ props.title }}</span>
+				</div>
+			</template>
+			<el-form :model="ruleForm" ref="ruleFormRef" label-width="auto" :rules="rules">
+				<el-row :gutter="35">
+					<el-form-item v-show="false">
+						<el-input v-model="ruleForm.id" />
+					</el-form-item>
+					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+						<el-form-item label="状态" prop="status">
+							<el-select clearable v-model="ruleForm.status" placeholder="请选择状态">
+								<el-option v-for="(item,index) in  getEnumStatusData" :key="index" :value="item.value" :label="`[${item.value}] ${item.describe}`"></el-option>
+								
+							</el-select>
+							
+						</el-form-item>
+						
+					</el-col>
+					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+						<el-form-item label="昵称" prop="name">
+							<el-input v-model="ruleForm.name" placeholder="请输入昵称" maxlength="64" show-word-limit clearable />
+							
+						</el-form-item>
+						
+					</el-col>
+					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+						<el-form-item label="描述" prop="description">
+							<el-input v-model="ruleForm.description" placeholder="请输入描述" maxlength="512" show-word-limit clearable />
+							
+						</el-form-item>
+						
+					</el-col>
+				</el-row>
+			</el-form>
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button @click="cancel">取 消</el-button>
+					<el-button type="primary" @click="submit">确 定</el-button>
+				</span>
+			</template>
+		</el-dialog>
+	</div>
+</template>
+<style scoped>
+:deep(.el-select),
+:deep(.el-input-number) {
+	width: 100%;
+}
+</style>
+<script lang="ts" setup>
+	import { ref,onMounted } from "vue";
+	import { getDictDataItem as di, getDictDataList as dl } from '/src/utils/dict-utils';
+	import { ElMessage } from "element-plus";
+	import type { FormRules } from "element-plus";
+	import { addGameUserEntity, updateGameUserEntity } from "/src/api/game/gameUserEntity";
+	import { getAPI } from '/src/utils/axios-utils';
+	import { SysEnumApi } from '/src/api-services/api';
+
+	const getEnumStatusData = ref<any>([]);
+	//父级传递来的参数
+	var props = defineProps({
+		title: {
+		type: String,
+		default: "",
+	},
+	});
+	//父级传递来的函数，用于回调
+	const emit = defineEmits(["reloadTable"]);
+	const ruleFormRef = ref();
+	const isShowDialog = ref(false);
+	const ruleForm = ref<any>({});
+	//自行添加其他规则
+	const rules = ref<FormRules>({
+		userName: [{required: true, message: '请输入账号！', trigger: 'blur',},],
+		password: [{required: true, message: '请输入密码！', trigger: 'blur',},],
+		name: [{required: true, message: '请输入昵称！', trigger: 'blur',},],
+	});
+
+	// 打开弹窗
+	const openDialog = (row: any) => {
+		ruleForm.value = JSON.parse(JSON.stringify(row));
+		isShowDialog.value = true;
+	};
+
+	// 关闭弹窗
+	const closeDialog = () => {
+		emit("reloadTable");
+		isShowDialog.value = false;
+	};
+
+	// 取消
+	const cancel = () => {
+		isShowDialog.value = false;
+	};
+
+	// 提交
+	const submit = async () => {
+		ruleFormRef.value.validate(async (isValid: boolean, fields?: any) => {
+			if (isValid) {
+				let values = ruleForm.value;
+				if (ruleForm.value.id == undefined || ruleForm.value.id == null || ruleForm.value.id == "" || ruleForm.value.id == 0) {
+					await addGameUserEntity(values);
+				} else {
+					await updateGameUserEntity(values);
+				}
+				closeDialog();
+			} else {
+				ElMessage({
+					message: `表单有${Object.keys(fields).length}处验证失败，请修改后再提交`,
+					type: "error",
+				});
+			}
+		});
+	};
+
+
+
+
+
+
+
+	// 页面加载时
+	onMounted(async () => {
+			getEnumStatusData.value = (await getAPI(SysEnumApi).apiSysEnumEnumDataListGet('GameUserStatus')).data.result ?? [];
+	});
+
+	//将属性或者函数暴露给父组件
+	defineExpose({ openDialog });
+</script>
+
+
+
+
---
+++ Web/src/views/game/gameuser/index.vue
@@ -0,0 +1,183 @@
+﻿<template>
+  <div class="gameuser-container">
+    <el-card shadow="hover" :body-style="{ paddingBottom: '0' }">
+      <el-form :model="queryParams" ref="queryForm" labelWidth="90">
+        <el-row>
+          <el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="4" class="mb10" v-if="showAdvanceQueryUI">
+            <el-form-item label="用户ID">
+              <el-input v-model="queryParams.id" clearable="" placeholder="请输入Id"/>
+              
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="4" class="mb10" v-if="showAdvanceQueryUI">
+            <el-form-item label="账号">
+              <el-input v-model="queryParams.userName" clearable="" placeholder="请输入账号"/>
+              
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="4" class="mb10" v-if="showAdvanceQueryUI">
+            <el-form-item label="状态">
+              <el-select clearable="" v-model="queryParams.status" placeholder="请选择状态">
+                <el-option v-for="(item,index) in getEnumStatusData_Index" :key="index" :value="item.value" :label="`[${item.value}]  ${item.name} ${item.describe}`" />
+                
+              </el-select>
+              
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="4" class="mb10" v-if="showAdvanceQueryUI">
+            <el-form-item label="昵称">
+              <el-input v-model="queryParams.name" clearable="" placeholder="请输入昵称"/>
+              
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="mb10">
+            <el-form-item>
+              <el-button-group>
+                <el-button type="primary"  icon="ele-Search" @click="handleQuery" v-auth="'gameuser:page'"> 查询 </el-button>
+                <el-button icon="ele-Refresh" @click="() => queryParams = {}"> 重置 </el-button>
+                
+              </el-button-group>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-card>
+    <el-card class="full-table" shadow="hover" style="margin-top: 8px">
+      <el-table
+				:data="tableData"
+				style="width: 100%"
+				v-loading="loading"
+				tooltip-effect="light"
+				row-key="id"
+				border="">
+        <el-table-column type="index" label="序号" width="55" align="center"/>
+        <el-table-column prop="id" label="用户ID" width="140" show-overflow-tooltip="" />
+        <el-table-column prop="userName" label="账号" width="140" show-overflow-tooltip="" />
+          <el-table-column prop="status" label="状态" width="140" show-overflow-tooltip="" >
+            <template #default="scope">
+              <el-tag>{{ getEnumDesc(scope.row.status, getEnumStatusData_Index)}}</el-tag>
+            </template>
+          </el-table-column>
+        <el-table-column prop="name" label="昵称" width="140" show-overflow-tooltip="" />
+        <el-table-column prop="description" label="描述" width="140" show-overflow-tooltip="" />
+        <el-table-column prop="updateUserName" label="最后更新人" width="140" show-overflow-tooltip="" />
+        <el-table-column label="操作" width="140" align="center" fixed="right" show-overflow-tooltip="" v-if="auth('gameuser:edit') || auth('gameuser:delete')">
+          <template #default="scope">
+            <el-button icon="ele-Edit" size="small" text="" type="primary" @click="openEditGameUserEntity(scope.row)" v-auth="'gameuser:edit'"> 编辑 </el-button>
+            <el-button icon="ele-Delete" size="small" text="" type="primary" @click="delGameUserEntity(scope.row)" v-auth="'gameuser:delete'"> 删除 </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+				v-model:currentPage="tableParams.page"
+				v-model:page-size="tableParams.pageSize"
+				:total="tableParams.total"
+				:page-sizes="[10, 20, 50, 100, 200, 500]"
+				small=""
+				background=""
+				@size-change="handleSizeChange"
+				@current-change="handleCurrentChange"
+				layout="total, sizes, prev, pager, next, jumper"
+	/>
+      <editDialog
+        ref="editDialogRef"
+        :title="editGameUserEntityTitle"
+        @reloadTable="handleQuery"
+      />
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts" setup="" name="gameuser">
+  import { ref } from "vue";
+  import { ElMessageBox, ElMessage } from "element-plus";
+  import { auth } from '/@/utils/authFunction';
+  import { getDictDataItem as di, getDictDataList as dl } from '/src/utils/dict-utils';
+  //import { formatDate } from '/@/utils/formatTime';
+
+  import editDialog from '/@/views/game/gameuser/component/editDialog.vue'
+  import { pageGameUserEntity, deleteGameUserEntity } from '/@/api/game/gameUserEntity';
+	import { getAPI } from '/@/utils/axios-utils';
+	import { SysEnumApi } from '/@/api-services/api';
+  import commonFunction from '/@/utils/commonFunction';
+
+  const getEnumStatusData_Index = ref<any>([]);
+
+	const { getEnumDesc } = commonFunction();
+  const showAdvanceQueryUI = ref(true);
+  const editDialogRef = ref();
+  const loading = ref(false);
+  const tableData = ref<any>([]);
+  const queryParams = ref<any>({});
+  const tableParams = ref({
+    page: 1,
+    pageSize: 10,
+    total: 0,
+  });
+  const editGameUserEntityTitle = ref("");
+
+  // 改变高级查询的控件显示状态
+  const changeAdvanceQueryUI = () => {
+    showAdvanceQueryUI.value = !showAdvanceQueryUI.value;
+  }
+  
+
+  // 查询操作
+  const handleQuery = async () => {
+    loading.value = true;
+    var res = await pageGameUserEntity(Object.assign(queryParams.value, tableParams.value));
+    tableData.value = res.data.result?.items ?? [];
+    tableParams.value.total = res.data.result?.total;
+    loading.value = false;
+    getEnumStatusData_Index.value = (await getAPI(SysEnumApi).apiSysEnumEnumDataListGet('GameUserStatus')).data.result ?? [];
+  };
+
+  // 打开新增页面
+  const openAddGameUserEntity = () => {
+    editGameUserEntityTitle.value = '添加游戏账号';
+    editDialogRef.value.openDialog({});
+  };
+
+  // 打开编辑页面
+  const openEditGameUserEntity = (row: any) => {
+    editGameUserEntityTitle.value = '编辑游戏账号';
+    editDialogRef.value.openDialog(row);
+  };
+
+  // 删除
+  const delGameUserEntity = (row: any) => {
+    ElMessageBox.confirm(`确定要删除吗?`, "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
+  .then(async () => {
+    await deleteGameUserEntity(row);
+    handleQuery();
+    ElMessage.success("删除成功");
+  })
+  .catch(() => {});
+  };
+
+  // 改变页面容量
+  const handleSizeChange = (val: number) => {
+    tableParams.value.pageSize = val;
+    handleQuery();
+  };
+
+  // 改变页码序号
+  const handleCurrentChange = (val: number) => {
+    tableParams.value.page = val;
+    handleQuery();
+  };
+
+  handleQuery();
+</script>
+<style scoped>
+:deep(.el-ipnut),
+:deep(.el-select),
+:deep(.el-input-number) {
+	width: 100%;
+}
+</style>
+
