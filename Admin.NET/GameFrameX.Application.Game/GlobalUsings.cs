3f684240f93c666ca1c16247951a436eb5dd413a
Blank <wangfj11@foxmail.com>
2024-01-25 10:42:48 +0800

    [增加]1. 增加游戏程序集的全局引用

--- Admin.NET/GameFrameX.Application.Game/GlobalUsings.cs
+++ Admin.NET/GameFrameX.Application.Game/GlobalUsings.cs
@@ -16,6 +16,7 @@ global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.DependencyInjection;
 global using SqlSugar;
+global using GameFrameX.Game.Enum;
 global using System;
 global using System.Collections.Generic;
 global using System.ComponentModel;
