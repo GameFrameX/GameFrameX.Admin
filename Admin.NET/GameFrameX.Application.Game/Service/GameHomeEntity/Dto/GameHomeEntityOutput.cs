4f1c38311c88ba1e0465ec1020f7e960a54467bf
Blank <wangfj11@foxmail.com>
2024-01-26 09:28:28 +0800

    [增加]1. 增加主页数据获取

---
+++ Admin.NET/GameFrameX.Application.Game/Service/GameHomeEntity/Dto/GameHomeEntityOutput.cs
@@ -0,0 +1,24 @@
+﻿// using GameFrameX.Game.Enum;
+//
+// namespace GameFrameX.Application.Game;
+//
+// /// <summary>
+// /// 游戏账号输出参数
+// /// </summary>
+// public class GameUserEntityOutput
+// {
+//     /// <summary>
+//     /// Id
+//     /// </summary>
+//     public long Id { get; set; }
+//     
+//     /// <summary>
+//     /// 账号
+//     /// </summary>
+//     public string Count { get; set; }
+//     
+//
+//     
+//     }
+//  
+//
---
+++ Web/src/api/home/gameHomeEntity.ts
@@ -0,0 +1,13 @@
+﻿import request from '/@/utils/request';
+
+enum Api {
+    GetOnlineGameHomeEntity = '/api/gameHomeEntity/GetOnline',
+}
+
+// 获取在线玩家
+export const getOnlineGameHomeEntity = (params?: any) =>
+    request({
+        url: Api.GetOnlineGameHomeEntity,
+        method: 'post',
+        data: params,
+    });
