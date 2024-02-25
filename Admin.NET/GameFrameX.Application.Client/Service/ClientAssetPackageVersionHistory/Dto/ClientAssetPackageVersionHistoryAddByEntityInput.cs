a857acafc0ea8cfa11ba8adb33e8c9559e859153
Blank <wangfj11@foxmail.com>
2024-02-25 13:25:02 +0800

    [修复]1. 修复文件丢失的编译报错

---
+++ Admin.NET/GameFrameX.Application.Client/Service/ClientAssetPackageVersionHistory/Dto/ClientAssetPackageVersionHistoryAddByEntityInput.cs
@@ -0,0 +1,36 @@
+﻿using GameFrameX.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameFrameX.Application.Client;
+
+/// <summary>
+/// 资源包版本记录增加输入参数
+/// </summary>
+public class AddClientAssetPackageVersionHistoryInput : BaseAddInput
+{
+    /// <summary>
+    /// 主键Id
+    /// </summary>
+    [Required(ErrorMessage = "主键Id不能为空")]
+    public long Id { get; set; }
+        
+    /// <summary>
+    /// 资源包版本Id
+    /// </summary>
+    [Required(ErrorMessage = "资源包版本Id不能为空")]
+    public long VersionId { get; set; }
+        
+    /// <summary>
+    /// 旧资源版本
+    /// </summary>
+    [Required(ErrorMessage = "旧资源版本不能为空")]
+    public string OldVersion { get; set; }
+        
+    /// <summary>
+    /// 新资源版本
+    /// </summary>
+    [Required(ErrorMessage = "新资源版本不能为空")]
+    public string Version { get; set; }
+        
+}
+
