b2f63f7defb918a86ad1dec420f1e90cf1faf8e3
Blank <wangfj11@foxmail.com>
2024-02-25 11:48:52 +0800

    [修改]1. 修改程序版本的代码标准

--- Admin.NET/Api/GameFrameX.Application.Client.Api/GameAppVersion/GameAppVersionRequest.cs
+++ Admin.NET/Api/GameFrameX.Application.Client.Api/GameAppVersion/GameAppVersionRequest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameFrameX.Application.Api.GameApplicationVersion;
 
 /// <summary>
@@ -8,15 +10,30 @@ public class GameAppVersionRequest
     /// <summary>
     /// 语言
     /// </summary>
+    [Required(AllowEmptyStrings = false)]
     public string Language { get; set; }
 
+    /// <summary>
+    /// 包名
+    /// </summary>
+    [Required(AllowEmptyStrings = false)]
     public string PackageName { get; set; }
+
+    /// <summary>
+    /// 程序版本
+    /// </summary>
+    [Required(AllowEmptyStrings = false)]
     public string AppVersion { get; set; }
+
+    /// <summary>
+    /// 主渠道名称
+    /// </summary>
+    [Required(AllowEmptyStrings = false)]
     public string Channel { get; set; }
-    public string SubChannel { get; set; }
 
     /// <summary>
     /// 平台
     /// </summary>
+    [Required(AllowEmptyStrings = false)]
     public string Platform { get; set; }
 }
