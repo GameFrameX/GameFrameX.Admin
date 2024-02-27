894054aaed108b1dac431b7baf174a0dc97be65a
Blank <wangfj11@foxmail.com>
2024-02-27 11:45:26 +0800

    [删除]1. 删除依赖功能。拆分项目

--- Admin.NET/GameFrameX.Application.Client/Service/ClientAssetPackageVersionHistory/Dto/ClientAssetPackageVersionHistoryOutput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameFrameX.Application.Client;
-
-/// <summary>
-/// 资源包版本记录输出参数
-/// </summary>
-public class ClientAssetPackageVersionHistoryOutput : ClientAssetPackageVersionHistory, IBasePageOutput<ClientAssetPackageVersionHistory>
-{
-
-}
--- Admin.NET/GameFrameX.Application.Game/Service/GameBannerAnnouncementEntity/Dto/GameBannerAnnouncementEntityOutput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameFrameX.Application.Game;
-
-/// <summary>
-/// 游戏内横幅公告输出参数
-/// </summary>
-public class GameBannerAnnouncementEntityOutput : GameBannerAnnouncementEntity, IBasePageOutput<GameBannerAnnouncementEntity>
-{
-
-}
